7480cb67d93334074bcd5f52a682d093c886592f
EvaD856 <148793536+EvaD856@users.noreply.github.com>
2024-09-17 14:53:15 +0000

    Started incomeTaxCalc.py

---
+++ incomeTaxCalc.py
@@ -0,0 +1,47 @@
+"""
+Title: Income Tax Calculator
+Author: Eva Donahue
+
+Pseudo-code: This is code like structure written in plain english.
+1. Input the gross income and number of dependents 
+2. Compute the taxable income using the formula: 
+Taxable income = gross income - 10,000 - (3,000 * number of dependents)
+
+3. Compute the income tax using the formula:
+Tax = taxable income * 0.20 
+4. Print the total tax 
+"""
+
+"""
+Better Pseudo-code:
+
+Compute a person's income tax.
+1. Significant constants:   # constant is a non-changing variable 
+        tax_rate 
+        standard_deduction
+        deduction_per_dependent
+2. The inputs are:
+        gross income
+        number of dependents 
+3. Computations
+        taxable income = the standard deduction - a deduction for each dependent
+        income tax = is a fixed percentage of taxable income
+4. The outputs are:
+        Your income tax
+"""
+
+
+# Initialize the constants 
+TAX_RATE = 0.20
+STANDARD_DEDUCTION = 10000.00 # FLOAT - and numerical value with a decimal
+DEPENDENT_DEDUCTION = 3000.00
+
+
+# Request the inputs from user 
+
+
+
+# Compute the income tax
+
+
+# Display the income tax 
